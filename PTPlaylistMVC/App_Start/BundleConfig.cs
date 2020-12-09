eae5675e00dc82e5af0a6ee339e55f7662b431f1
Philip Holt <philipholt14@gmail.com>
2020-12-08 23:04:14 -0800

    Added in my terrible CSS. :P

--- PTPlaylistMVC/App_Start/BundleConfig.cs
+++ PTPlaylistMVC/App_Start/BundleConfig.cs
@@ -30,6 +30,10 @@ namespace PTPlaylistMVC
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/ptpCss").Include(
+                    "~/Content/plainTextPlaylistStyles.css"
+            ));
         }
     }
 }
