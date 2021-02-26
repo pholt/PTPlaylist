b88f4559f7bb52ed46408e04d7ba7be250689c82
Philip Holt <philipholt14@gmail.com>
2021-02-26 11:42:18 -0800

    No longer pulling API key into view. :|

--- PTPlaylistMVC/Controllers/HomeController.cs
+++ PTPlaylistMVC/Controllers/HomeController.cs
@@ -8,14 +8,9 @@ namespace PTPlaylistMVC.Controllers
 {
     public class HomeController : Controller
     {
-        static string API_KEY = System.Web.Configuration.WebConfigurationManager.AppSettings["API_KEY"];
-
         public ActionResult Index()
         {
-            ViewBag.API_KEY = API_KEY;
             return View();
         }
-
-        // TODO: API action to get YouTube video ID from first result. Currently this is handled through js, but should instead be handled through server calls
     }
 }
