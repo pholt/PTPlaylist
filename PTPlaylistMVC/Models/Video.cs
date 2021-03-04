23e607e6acf6086e14062461c722c9efb0bc1f60
Philip Holt <philipholt14@gmail.com>
2021-03-04 11:34:31 -0800

    Added CosmoDB NuGet package and Video data model.

---
+++ PTPlaylistMVC/Models/Video.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace PTPlaylistMVC.Models
+{
+    public class Video
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "query")]
+        public string Query { get; set; }
+
+        [JsonProperty(PropertyName = "videoId")]
+        public string VideoId { get; set; }
+
+        [JsonProperty(PropertyName = "refreshDate")]
+        public DateTime RefreshDate { get; set; }
+    }
+}
