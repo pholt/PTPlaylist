002c7774961257e4b5837e91c50c023c648ed633
Philip Holt <philipholt14@gmail.com>
2021-03-04 23:56:01 -0800

    CosmosDB instantiation plus some TODOs.

--- PTPlaylistMVC/Controllers/DataController.cs
+++ PTPlaylistMVC/Controllers/DataController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PTPlaylistMVC.Interfaces;
+using PTPlaylistMVC.Services;
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
@@ -9,12 +11,23 @@ namespace PTPlaylistMVC.Controllers
     [Route("api/data")]
     public class DataController : ApiController
     {
+        private static readonly ICosmosDbService _cosmosDbService = new CosmosDbService(
+            new Microsoft.Azure.Cosmos.CosmosClient(
+                System.Web.Configuration.WebConfigurationManager.AppSettings["COSMOS_ACCOUNT"], 
+                System.Web.Configuration.WebConfigurationManager.AppSettings["COSMOS_KEY"]),
+            System.Web.Configuration.WebConfigurationManager.AppSettings["COSMOS_DB_NAME"],
+            System.Web.Configuration.WebConfigurationManager.AppSettings["COSMOS_DB_CONTAINER_NAME"]);
+
         private static readonly string API_KEY = System.Web.Configuration.WebConfigurationManager.AppSettings["API_KEY"];
         private static readonly HttpClient client = new HttpClient();
 
         // GET api/data/{query}
         public async Task<string> GetAsync(string query)
         {
+            // TODO: Try DB first for entry, if it exists, pull the video id from there and check YouTube if it's still valid
+            //  if it doesn't exist in the DB, query YouTube for it, store it in DB
+            // finally, return the id
+
             // This query fetches a single result from the YouTubeV3 API.
             string urlBase = "https://www.googleapis.com/youtube/v3/search?key={0}&q={1}&part=snippet&maxResults=1&safeSearch=none&type=video&videoEmbeddable=true";
             string formattedUrl = String.Format(urlBase, API_KEY, HttpUtility.UrlEncode(query));
@@ -22,6 +35,7 @@ namespace PTPlaylistMVC.Controllers
             try
             {
                 string response = await client.GetStringAsync(formattedUrl);
+                // TODO: Client really only needs video id and video name, this data should be broken down here
                 return response;
             }
             catch (Exception ex)
