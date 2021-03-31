8521129557ab908285faf78b05b41a988213c40a
Philip Holt <philipholt14@gmail.com>
2021-03-31 13:21:06 -0700

    Reorganizing tests.

---
+++ PTPlaylistTests/MockObjects/MockCosmosDbService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PTPlaylistMVC.Interfaces;
+using PTPlaylistMVC.Models;
+
+namespace PTPlaylistTests.MockObjects
+{
+    class MockCosmosDbService : ICosmosDbService
+    {
+        public async Task AddVideoAsync(Video video)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteVideoAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Video> GetVideoAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Video>> GetVideosAsync(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UpdateVideoAsync(string id, Video video)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PTPlaylistTests/MockObjects/MockYoutubeService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PTPlaylistMVC.Interfaces;
+
+namespace PTPlaylistTests.MockObjects
+{
+    class MockYoutubeService : IYoutubeService
+    {
+        public async Task<string> GetSingleVideoId(string query)
+        {
+            return "mockVideoId";
+        }
+
+        public async Task<bool> IsYouTubeVideoPlayable(string videoId)
+        {
+            return videoId == "true";
+        }
+    }
+}
